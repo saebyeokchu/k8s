9c4e6582325067c70c7b302551a5fcfe0189ab74
bydawn <62323870+saebyeokchu@users.noreply.github.com>
2023-09-17 10:41:04 +0900

    simple nodejs app for test

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('{ "response": "Dawn is trying" }');
+});
+
+app.get('/hello', function (req, res) {
+    res.send('{ "response": "Hello World" }');
+});
+app.get('/ready', function (req, res) {
+    res.send('{ "response": " Are you ready?" }');
+});
+
+app.listen(process.env.PORT || 3000);
+module.exports = app;
